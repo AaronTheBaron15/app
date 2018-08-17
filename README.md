223f6be184f7a237847f092d8f0e8d5d6debdd44
Aaron Borjas <aaborjas017@gmail.com>
2018-08-17 14:29:30 -0700

    Added .babelrc and node modules

--- README.md
+++
@@ -1,2 +0,0 @@
-# app
-app using react and redux 
--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -1,24 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux'; 
 
-export default () => {
-    return (
-        <nav className="navbar navbar-default">
-            <div className="container-fluid">
-                <div className="navbar-header">
-                    <Link to="/" className="navbar-brand">display_cms</Link>
+class NavigationBar extends React.Component {
+    render() {
+        const { isAuthenticated } = this.props.auth;
+
+        const userLinks = (
+            <ul className="nav navbar-nav navbar-right">
+                <li><a href="#">Logout</a></li>
+            </ul>
+        );
+
+        const guestLinks = (
+            <ul className="nav navbar-nav navbar-right">
+                <li><Link to="/signup"> Sign up </Link> </li>
+                <li><Link to="/login"> Login </Link> </li>
+            </ul>
+        );
+
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <Link to="/" className="navbar-brand">display_cms</Link>
+                    </div>
+
+                    <div className="collapse navbar-collapse">
+                        { isAuthenticated ? userLinks : guestLinks }
+                    </div>
+
                 </div>
 
-                <div className="collapse navbar-collapse">
-                    <ul className="nav navbar-nav navbar-right">
-                        <li><Link to="/signup"> Sign up </Link> </li>
-                        <li><Link to="/login"> Login </Link> </li>
-                    </ul>
-
-                </div>
-
-            </div>
-
-        </nav>
-    );
+            </nav>
+        );
+    }
 }
+
+NavigationBar.propTypes = {
+    auth: React.PropTypes.object.isRequired
+}
+
+function mapStateToProps(state) {
+    return {
+        auth: state.auth
+    };
+}
+
+export default connect(mapStateToProps)(NavigationBar);
