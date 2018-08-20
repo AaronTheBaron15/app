b121c8d05c09d74e4d55558d80503edad4ec86b2
Aaron Borjas <aaborjas017@gmail.com>
2018-08-20 09:24:41 -0700

    login and logout on client

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux'; 
+import { timingSafeEqual } from 'crypto';
+import { logout } from '../actions/authActions'; 
 
 class NavigationBar extends React.Component {
+    logout(e) {
+        this.props.logout();
+    }
+
     render() {
         const { isAuthenticated } = this.props.auth;
 
         const userLinks = (
             <ul className="nav navbar-nav navbar-right">
-                <li><a href="#">Logout</a></li>
+                <li><a href="#" onClick={this.logout.bind(this)}>Logout</a></li>
             </ul>
         );
 
@@ -38,7 +44,8 @@ class NavigationBar extends React.Component {
 }
 
 NavigationBar.propTypes = {
-    auth: React.PropTypes.object.isRequired
+    auth: React.PropTypes.object.isRequired,
+    logout: React.PropTypes.func.isRequired
 }
 
 function mapStateToProps(state) {
@@ -47,4 +54,4 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps)(NavigationBar);
+export default connect(mapStateToProps, { logout })(NavigationBar);
