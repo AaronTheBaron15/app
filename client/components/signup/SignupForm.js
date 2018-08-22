ace5b2b3a30d2969f8e68e1ddb0dfbf7601f2817
Aaron Borjas <aaborjas017@gmail.com>
2018-08-22 10:35:03 -0700

    edit signupform

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -2,161 +2,156 @@ import React from 'react';
 import timezones from '../../data/timezones';
 import map from 'lodash/map';
 import classnames from 'classnames';
-import { timingSafeEqual } from 'crypto';
 import validateInput from '../../../server/shared/validations/signup';
 import TextFieldGroup from '../common/TextFieldGroup';
-import { addFlashMessage } from '../../actions/flashMessages';
 
 class SignupForm extends React.Component {
-    constructor (props) {
-        super (props);
-        this.state = {
-            username: '',
-            email: '',
-            password: '',
-            passwordConfirmation: '',
-            timezone: '',
-            errors: {},
-            isLoading: false,
-            isInvalid: false
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+      passwordConfirmation: '',
+      timezone: '',
+      errors: {},
+      isLoading: false,
+      invalid: false
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.checkUserExists = this.checkUserExists.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  isValid() {
+    const { errors, isValid } = validateInput(this.state);
+
+    if (!isValid) {
+      this.setState({ errors });
+    }
+
+    return isValid;
+  }
+
+  checkUserExists(e) {
+    const field = e.target.name;
+    const val = e.target.value;
+    if (val !== '') {
+      this.props.isUserExists(val).then(res => {
+        let errors = this.state.errors;
+        let invalid;
+        if (res.data.user) {
+          errors[field] = 'There is user with such ' + field;
+          invalid = true;
+        } else {
+          errors[field] = '';
+          invalid = false;
         }
-
-        this.onChange = this.onChange.bind (this);
-        this.onSubmit = this.onSubmit.bind (this);
-        this.checkUserExists = this.checkUserExists.bind (this);
+        this.setState({ errors, invalid });
+      });
     }
+  }
 
-    onChange (e) {
-        this.setState ({ [e.target.name]: e.target.value })
+  onSubmit(e) {
+    e.preventDefault();
+
+    if (this.isValid()) {
+      this.setState({ errors: {}, isLoading: true });
+      this.props.userSignupRequest(this.state).then(
+        () => {
+          this.props.addFlashMessage({
+            type: 'success',
+            text: 'You signed up successfully. Welcome!'
+          });
+          this.context.router.push('/');
+        },
+        (err) => this.setState({ errors: err.response.data, isLoading: false })
+      );
     }
+  }
 
-    isValid() {
-        const { errors, isValid } = validateInput(this.state);
+  render() {
+    const { errors } = this.state;
+    const options = map(timezones, (val, key) =>
+      <option key={val} value={val}>{key}</option>
+    );
+    return (
+      <form onSubmit={this.onSubmit}>
+        <h1>Join our community!</h1>
 
-        if(!isValid) {
-            this.setState({ errors });
-        }
-        
-        return isValid;
-    }
+        <TextFieldGroup
+          error={errors.username}
+          label="Username"
+          onChange={this.onChange}
+          checkUserExists={this.checkUserExists}
+          value={this.state.username}
+          field="username"
+        />
 
-    checkUserExists(e) {
-        const field = e.target.name;
-        const val = e.target.value;
+        <TextFieldGroup
+          error={errors.email}
+          label="Email"
+          onChange={this.onChange}
+          checkUserExists={this.checkUserExists}
+          value={this.state.email}
+          field="email"
+        />
 
-        if(val !== '') {
-            this.props.isUserExists(val).then(res => {
-                let errors = this.state.errors;
-                let invalid;
-                if(res.data.user) {
-                    errors[field] = 'There is a user with such ' + field;
-                    invalid = true;
-                } else {
-                    errors[field] = '';
-                    invalid = false;
-                }
-                this.setState({ errors, invalid }) 
-            });
-        }
-    }
+        <TextFieldGroup
+          error={errors.password}
+          label="Password"
+          onChange={this.onChange}
+          value={this.state.password}
+          field="password"
+          type="password"
+        />
 
-    onSubmit (e) {
-        e.preventDefault();
-        
-        if(true) {
-            this.setState ({ errors: {}, isLoading: true });
-            e.preventDefault ();
-            this.props.userSignupRequest (this.state).then (
-                () => {
-                    this.props.addFlashMessage({
-                        type: 'success',
-                        text: 'You signed up successfully. Welcome!'
-                    });
-                    this.context.router.push('/');
-                },
-                ({ data }) => this.setState ({ errors: data, isLoading: false })
-            );
-        }
-    }
+        <TextFieldGroup
+          error={errors.passwordConfirmation}
+          label="Password Confirmation"
+          onChange={this.onChange}
+          value={this.state.passwordConfirmation}
+          field="passwordConfirmation"
+          type="password"
+        />
 
-    render () {
-        const { errors } = this.state;
-        const options = map (timezones, (val, key) =>
-            <option key={val} value={val}>{key}</option>
-);
-        return (
-            <form onSubmit={this.onSubmit}>
-                <h1>Join our community!</h1>
+        <div className={classnames("form-group", { 'has-error': errors.timezone })}>
+          <label className="control-label">Timezone</label>
+          <select
+            className="form-control"
+            name="timezone"
+            onChange={this.onChange}
+            value={this.state.timezone}
+          >
+            <option value="" disabled>Choose Your Timezone</option>
+            {options}
+          </select>
+          {errors.timezone && <span className="help-block">{errors.timezone}</span>}
+        </div>
 
-                <TextFieldGroup
-                    error={errors.username}
-                    label="Username"
-                    onChange={this.onChange}
-                    checkUserExists={this.checkUserExists}
-                    value={this.state.username}
-                    field="username"
-                />
-
-                <TextFieldGroup
-                    error={errors.email}
-                    label="Email"
-                    onChange={this.onChange}
-                    checkUserExists={this.checkUserExists}
-                    value={this.state.email}
-                    field="email"
-                />
-
-                <TextFieldGroup
-                    error={errors.password}
-                    label="Password"
-                    onChange={this.onChange}
-                    value={this.state.password}
-                    field="password"
-                    type="password"
-                />
-
-                <TextFieldGroup
-                    error={errors.passwordConfirmation}
-                    label="Password Confirmation"
-                    onChange={this.onChange}
-                    value={this.state.passwordConfirmation}
-                    field="passwordConfirmation"
-                    type="password"
-                />
-
-                <div className={classnames("form-group", { 'has-error': errors.timezone })}>
-                    <label className="control-label">Timezone</label>
-                    <select
-                        value = {this.state.timezone}
-                        onChange = {this.onChange}
-                        name="timezone"
-                        className="form-control"
-                    >
-                    <option value="" disabled>Choose Your Timezone</option>
-                    {options}
-                    </select>
-                    {errors.timezone && <span className="help-block">{errors.timezone}</span>}
-                </div>
-
-                <div className="form-group">
-                    <button disabled={this.state.isLoading || this.state.invalid } className="btn btn-primary btn-lg">
-                      Sign up
-                    </button>
-                </div>
-
-            </form>
-)
-    }
+        <div className="form-group">
+          <button disabled={this.state.isLoading || this.state.invalid} className="btn btn-primary btn-lg">
+            Sign up
+          </button>
+        </div>
+      </form>
+    );
+  }
 }
 
 SignupForm.propTypes = {
-    userSignupRequest: React.PropTypes.func.isRequired,
-    addFlashMessage: React.PropTypes.func.isRequired,
-    isUserExists: React.PropTypes.func.isRequired
+  userSignupRequest: React.PropTypes.func.isRequired,
+  addFlashMessage: React.PropTypes.func.isRequired,
+  isUserExists: React.PropTypes.func.isRequired
 }
 
 SignupForm.contextTypes = {
-    router: React.PropTypes.object.isRequired
+  router: React.PropTypes.object.isRequired
 }
 
 export default SignupForm;
